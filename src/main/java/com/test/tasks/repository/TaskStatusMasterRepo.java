6e7b267fc7fbb7ae2afdb85320f32e65594fa546
supplecabinet <sudhanshukr98@gmail.com>
2024-03-30 10:24:57 +0530

    Removed unwanted entity and repo
    Removed unwanted entity and repo

--- src/main/java/com/test/tasks/repository/TaskStatusMasterRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.tasks.repository;
-
-import com.test.tasks.model.TaskStatusMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskStatusMasterRepo extends JpaRepository<TaskStatusMaster,Integer> {
-}
