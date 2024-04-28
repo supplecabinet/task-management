ea62a0bacc34bbefbcc309e8b7a8690f15615b2b
supplecabinet <sudhanshukr98@gmail.com>
2024-04-28 20:25:36 +0530

    Made application deployable on tomcat and added pom.xml
    Made application deployable on tomcat and added pom.xml

--- src/main/java/com/test/tasks/TaskManagementApplication.java
+++ src/main/java/com/test/tasks/TaskManagementApplication.java
@@ -3,12 +3,14 @@ package com.test.tasks;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableEncryptableProperties
-public class TaskManagementApplication {
+public class TaskManagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementApplication.class, args);
@@ -22,5 +24,9 @@ public class TaskManagementApplication {
 		loggingFilter.setIncludePayload(false);
 		loggingFilter.setMaxPayloadLength(64000);
 		return loggingFilter;
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TaskManagementApplication.class);
 	}
 }
